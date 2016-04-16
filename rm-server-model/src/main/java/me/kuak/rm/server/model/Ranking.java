885920247d3126d506fa3b45bd8dab84d17b94fd
garfenter <garfenter@gmail.com>
2016-04-16 09:24:26 -0600

    Fix ranking values

--- rm-server-model/src/main/java/me/kuak/rm/server/model/Ranking.java
+++ rm-server-model/src/main/java/me/kuak/rm/server/model/Ranking.java
@@ -14,16 +14,17 @@ import javax.persistence.NamedNativeQuery;
 @Entity
 @NamedNativeQueries({
     @NamedNativeQuery(query
-            = "SELECT g.id, rog.name, rog.description, SUM(qa.points) points\n"
+            = "SELECT g.id, g.user_ as name, rog.description, rco.name as country, SUM(qa.points) points\n"
             + "FROM rmdb.question_answer qa\n"
             + "INNER JOIN rmdb.question q ON (qa.question = q.id)\n"
             + "INNER JOIN rmdb.group_ g ON (g.id = qa.group_)\n"
             + "INNER JOIN rmdb.rally_country rc ON (rc.id = q.rally_country)\n"
             + "INNER JOIN rmdb.rally_object rog ON (rog.id = g.id)\n"
+            + "INNER JOIN rmdb.rally_object rco ON (rco.id = g.country)\n"
             + "WHERE \n"
             + "	rc.rally = ?rallyId\n"
             + "GROUP BY\n"
-            + "	g.id, rog.name, rog.description", name = "Ranking.findRankingsByRallyId", resultClass = Ranking.class)
+            + "	g.id, rog.name, rco.name, rog.description", name = "Ranking.findRankingsByRallyId", resultClass = Ranking.class)
 })
 public class Ranking implements Serializable {
 
@@ -31,9 +32,11 @@ public class Ranking implements Serializable {
     @Column(name = "id")
     private Integer id;
     @Column(name = "name")
-    private String name;
+    private String group;
     @Column(name = "description")
     private String description;
+    @Column(name = "country")
+    private String country;
     @Column(name = "points")
     private Integer points;
 
@@ -45,12 +48,12 @@ public class Ranking implements Serializable {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getGroup() {
+        return group;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setGroup(String group) {
+        this.group = group;
     }
 
     public String getDescription() {
@@ -67,6 +70,14 @@ public class Ranking implements Serializable {
 
     public void setPoints(Integer points) {
         this.points = points;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
 }
